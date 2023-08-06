971c92484bb730840d63d065adc3818cb519dd9c
aptt <git@aptt.ch>
2023-08-06 21:45:48 +0200

    feat(app): 🎸 add css-based variables & document styles

--- packages/app/src/routes/+layout.svelte
+++ packages/app/src/routes/+layout.svelte
@@ -2,6 +2,12 @@
   import '@unocss/reset/tailwind.css'
 
   import '@fontsource/manrope'
+
+  import '$css/colors.variables.css'
+  import '$css/content_margin.variables.css'
+
+  import '$css/document.css'
+  import '$css/svg.css'
 </script>
 
 <slot />
