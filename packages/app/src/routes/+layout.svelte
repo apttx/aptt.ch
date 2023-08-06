c00b54e3a4458c7f22e898b3f32b0af3bdc88dc5
aptt <git@aptt.ch>
2023-08-06 21:58:17 +0200

    feat(app): 🎸 import 600 & 800 weights, & 400 specifically

--- packages/app/src/routes/+layout.svelte
+++ packages/app/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import '@unocss/reset/tailwind.css'
 
-  import '@fontsource/manrope'
+  import '@fontsource/manrope/400.css'
+  import '@fontsource/manrope/600.css'
+  import '@fontsource/manrope/800.css'
 
   import '$css/colors.variables.css'
   import '$css/content_margin.variables.css'
