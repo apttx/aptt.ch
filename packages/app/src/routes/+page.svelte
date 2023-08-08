2cfdaede225fc98665f472a77a6a7430054a58f9
aptt <git@aptt.ch>
2023-08-08 18:20:50 +0200

    feat(app): 🎸 query familiarity & enjoyment to display tech as graph

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,32 +1,23 @@
 <script>
+  import { get_force_simulation } from '$utilities/simulation.mjs'
+
   import Browser_Window from '$components/browser_window.svelte'
   import Graph from '$components/graph.svelte'
-  import { get_force_simulation } from '$utilities/simulation.mjs'
+  import Technology_Graph from '$components/technology_graph.svelte'
 
   export let data
 </script>
 
+<div role="presentation">
+  <div
+    role="presentation"
+    class="technology_graph"
+  >
+    <Technology_Graph technologies={data.technologies} />
+  </div>
+</div>
+
 <h1>aptt</h1>
-
-<h2>about me</h2>
-
-<h2>stuff i've worked with</h2>
-
-<ul class="technology_list">
-  {#each data.technologies as technology}
-    <li class="technology_list_item">
-      <span>{technology.name}</span>
-
-      {#if technology.icon}
-        <img
-          src={technology.icon}
-          alt="{technology.name} logo"
-          class="technology_icon"
-        />
-      {/if}
-    </li>
-  {/each}
-</ul>
 
 <h2>stuff i've worked on</h2>
 
@@ -74,21 +65,9 @@
 {/each}
 
 <style>
-  .technology_list {
-    display: grid;
-    grid-auto-flow: column;
+  .technology_graph {
     margin-inline: var(--margin_content_layout);
     margin-block: 4rem;
-  }
-
-  .technology_list_item {
-    display: grid;
-    gap: 1rem;
-  }
-
-  .technology_icon {
-    width: auto;
-    height: 4rem;
   }
 
   .project_browser_window {
