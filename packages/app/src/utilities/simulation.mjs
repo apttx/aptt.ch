b23e1b38723de9d804a6181905fc34f0a595a35b
aptt <git@aptt.ch>
2023-08-09 22:19:05 +0200

    feat(app): 🎸 set 0.5 link force strength for smoother movement

--- packages/app/src/utilities/simulation.mjs
+++ packages/app/src/utilities/simulation.mjs
@@ -44,6 +44,7 @@ export const get_force_simulation = (options) => {
     // @ts-ignore this type is wrong. the same links are passed to function values, meaning that they initially have string sources & targets, then the given nodes (objects) after they were resolved.
     .distance(distance)
     .id((node) => node.id)
+    .strength(0.5)
   const charge_force = forceManyBody().strength(-100)
   const simulation = retypedForceSimulation(nodes)
     .force('link', link_force)
