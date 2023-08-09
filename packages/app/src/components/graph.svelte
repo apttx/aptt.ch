07c5b31ee1db64f165ca14d52d2e2daef027baa1
aptt <git@aptt.ch>
2023-08-09 22:27:40 +0200

    feat(app): 🎸 add node drag & click events to graph

--- packages/app/src/components/graph.svelte
+++ packages/app/src/components/graph.svelte
@@ -1,5 +1,6 @@
 <script>
   import { get_circle, get_line } from '$utilities/graph_svg.mjs'
+  import { createEventDispatcher } from 'svelte'
 
   /** @type {Simulation_Node[]} */
   export let nodes = []
@@ -16,12 +17,72 @@
 
   $: lines = edges.map((link) => get_line(link))
   $: circles = nodes.map((node) => get_circle(node))
+
+  /**
+   * @type {import('svelte').EventDispatcher<{
+   *   node_dragstart: { x: number; y: number }
+   *   node_drag: { x: number; y: number }
+   *   node_dragend: { x: number; y: number }
+   *   node_click: Simulation_Node
+   * }>}
+   */
+  const dispatch = createEventDispatcher()
+
+  // `dragstart`'s default behaviour has the svg move with the mouse. preventing default prevents `drag` events from firing for some reason.
+  /** @type {boolean} */
+  let mouse_pressed = false
+  /** @type {SVGElement} */
+  let svg_element
+
+  /** @type {(event: MouseEvent) => { x: number; y: number }} */
+  const get_svg_coordinates = (event) => {
+    const svg_rect = svg_element.getBoundingClientRect()
+
+    const { top, left, bottom, right } = svg_rect
+
+    const x = (((event.x - left - right + event.x) / svg_rect.width) * width) / 2
+    const y = (((event.y - top - bottom + event.y) / svg_rect.height) * height) / 2
+
+    const coordinates = { x, y }
+
+    return coordinates
+  }
 </script>
 
+<svelte:window
+  on:mousedown={(event) => {
+    mouse_pressed = true
+
+    const event_detail = get_svg_coordinates(event)
+
+    dispatch('node_dragstart', event_detail)
+  }}
+  on:mousemove={(event) => {
+    if (!mouse_pressed) {
+      return
+    }
+
+    const event_detail = get_svg_coordinates(event)
+
+    dispatch('node_drag', event_detail)
+  }}
+  on:mouseup={(event) => {
+    mouse_pressed = false
+
+    const event_detail = get_svg_coordinates(event)
+
+    dispatch('node_dragend', event_detail)
+  }}
+/>
+
 <svg
+  bind:this={svg_element}
   {width}
   {height}
   viewBox="{-width / 2} {-height / 2} {width} {height}"
+  role="toolbar"
+  tabindex="0"
+  on:dragstart|preventDefault
 >
   <g>
     {#each lines as line}
@@ -57,6 +118,14 @@
         style:--center-x="{circle.props.cx}px"
         style:--center-y="{circle.props.cy}px"
         class="node_image"
+        role="button"
+        tabindex="0"
+        on:click={() => {
+          dispatch('node_click', circle.node)
+        }}
+        on:keydown={() => {
+          dispatch('node_click', circle.node)
+        }}
       />
     {/each}
   </g>
@@ -67,6 +136,7 @@
     display: block;
     width: 100%;
     height: auto;
+    overflow: visible;
   }
 
   line {
