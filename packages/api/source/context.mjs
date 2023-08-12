80d311c88763114f2364ad0ccc6b878036e95a7a
aptt <git@aptt.ch>
2023-08-12 18:05:06 +0200

    fix(api): 🐛 read user from request auth header in env-based context

--- packages/api/source/context.mjs
+++ packages/api/source/context.mjs
@@ -1,25 +1,48 @@
+import assert from 'node:assert'
 import { env } from 'node:process'
 import { parse } from 'devalue'
 
-/** @type {() => () => Resolver_Context} */
-export const context_from_env = () => {
-  let projects = []
-  let technologies = []
+import { read_dotenv } from './environment.mjs'
+import { get_user_from_request } from './handler.mjs'
 
+const environment = read_dotenv()
+
+const jwt_secret = environment.PRIVATE_JWT_SECRET
+assert(
+  jwt_secret,
+  'missing `PRIVATE_JWT_SECRET` in environment.\n  ~~> $ echo PRIVATE_JWT_SECRET=replace_this >> .env',
+)
+
+/**
+ * @type {() => (
+ *   initial_context: import('graphql-yoga').YogaInitialContext,
+ * ) => Resolver_Context}
+ */
+export const context_from_env = () => {
+  /** @type {Restricted_Project[]} */
+  let projects = []
   if (env.PRIVATE_PROJECTS) {
     projects = parse(env.PRIVATE_PROJECTS)
   }
 
+  /** @type {Technology[]} */
+  let technologies = []
   if (env.PRIVATE_TECHNOLOGIES) {
     technologies = parse(env.PRIVATE_TECHNOLOGIES)
   }
 
-  const context = {
-    projects,
-    technologies,
-  }
+  /** @type {(initial_context: import('graphql-yoga').YogaInitialContext) => Resolver_Context} */
+  const context_function = (initial_context) => {
+    const user = get_user_from_request({ jwt_secret }, initial_context.request)
 
-  const context_function = () => context
+    const context = {
+      projects,
+      technologies,
+      user,
+    }
+
+    return context
+  }
 
   return context_function
 }
