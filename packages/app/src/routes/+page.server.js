1b220215f8caeb1bb2159e3266bdb2a1c9ae2727
aptt <git@aptt.ch>
2023-08-13 16:10:44 +0200

    feat(app): 🎸 query & display activities, & type page data query

--- packages/app/src/routes/+page.server.js
+++ packages/app/src/routes/+page.server.js
@@ -2,22 +2,7 @@ import { gql } from '@urql/core'
 import { client } from '../graphql.mjs'
 import { error } from '@sveltejs/kit'
 
-/**
- * @typedef {{
- *   type: 'edge'
- *   source: string
- *   target: string
- *   icon: string
- * }} Technology_Edge
- */
-
-/**
- * @typedef {{
- *   id: string
- *   type: 'node'
- *   icon: string
- * }} Technology_Node
- */
+/** @typedef {Pick<Technology, 'name' | 'icon' | 'enjoyment' | 'familiarity'>} Technology_Fragment */
 
 const technology_fragment = gql`
   fragment technology_fragment on Technology {
@@ -28,6 +13,49 @@ const technology_fragment = gql`
   }
 `
 
+/**
+ * @type {import('@urql/core').TypedDocumentNode<{
+ *   technologies: Technology_Fragment[]
+ *   projects: {
+ *     title: string
+ *     description: string
+ *     thumbnail: { url: string }
+ *     url: string
+ *     technologies: (
+ *       | Technology_Fragment
+ *       | { technologies: Technology_Fragment }
+ *       | {
+ *           connector?: Technology_Fragment
+ *           source: Technology_Fragment
+ *           target: Technology_Fragment
+ *         }
+ *     )[]
+ *   }[]
+ *   activities: {
+ *     title: string
+ *     place: {
+ *       name: string
+ *       icon?: string
+ *     }
+ *     date:
+ *       | {
+ *           iso_8601: string
+ *         }
+ *       | {
+ *           start: {
+ *             iso_8601: string
+ *           }
+ *           end?: { iso_8601: string }
+ *         }
+ *     events: {
+ *       title: string
+ *       date: {
+ *         iso_8601: string
+ *       }
+ *     }[]
+ *   }[]
+ * }>}
+ */
 const projects_query = gql`
   {
     technologies {
@@ -64,6 +92,33 @@ const projects_query = gql`
         }
       }
     }
+
+    activities {
+      title
+      place {
+        name
+        icon
+      }
+      date {
+        ... on Date {
+          iso_8601
+        }
+        ... on Date_Range {
+          start {
+            iso_8601
+          }
+          end {
+            iso_8601
+          }
+        }
+      }
+      events {
+        title
+        date {
+          iso_8601
+        }
+      }
+    }
   }
   ${technology_fragment}
 `
@@ -84,13 +139,57 @@ export const load = async (event) => {
     throw error(500, result.error.message)
   }
 
-  /** @type {Project[]} */
+  if (!result.data) {
+    throw error(500)
+  }
+
   const projects = result.data.projects
-  /** @type {Technology[]} */
   const technologies = result.data.technologies
+  const activities = result.data.activities.map((activity_data) => {
+    const { title, place } = activity_data
+
+    let date
+    if ('iso_8601' in activity_data.date) {
+      date = new Date(activity_data.date.iso_8601)
+    } else {
+      const start = new Date(activity_data.date.start.iso_8601)
+      let end
+      if (activity_data.date.end) {
+        end = new Date(activity_data.date.end.iso_8601)
+      }
+
+      date = {
+        start,
+        end,
+      }
+    }
+
+    const events = activity_data.events.map((event_data) => {
+      const { title } = event_data
+
+      const date = new Date(event_data.date.iso_8601)
+
+      const event = {
+        title,
+        date,
+      }
+
+      return event
+    })
+
+    const activity = {
+      title,
+      date,
+      events,
+      place,
+    }
+
+    return activity
+  })
 
   return {
     projects,
     technologies,
+    activities,
   }
 }
