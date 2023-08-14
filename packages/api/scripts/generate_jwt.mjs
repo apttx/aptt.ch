df479eafac10c8ad8b6c6cad6ca1e8c3cf50a92e
aptt <git@aptt.ch>
2023-08-14 21:55:10 +0200

    feat(api): 🎸 add expiration argument to jwt generation script

--- packages/api/scripts/generate_jwt.mjs
+++ packages/api/scripts/generate_jwt.mjs
@@ -19,6 +19,23 @@ const get_roles = (argument_variable) => {
   return roles
 }
 
+/** @type {(argument_variable: string[]) => Date} */
+const get_expiration = (argument_variable) => {
+  const index_of_dash_dash_roles = argument_variable.indexOf('--expiration')
+
+  if (index_of_dash_dash_roles === -1) {
+    throw 'missing `--expiration <date, yyyy-mm-dd>`'
+  }
+
+  const date_string = argument_variable[index_of_dash_dash_roles + 1]
+  const date_attribute_strings = date_string.split('-')
+  const [year, month, day] = date_attribute_strings.map((string) => parseInt(string))
+
+  const expiration = new Date(Date.UTC(year, month, day))
+
+  return expiration
+}
+
 const main = () => {
   const environment = read_dotenv()
 
@@ -34,12 +51,14 @@ const main = () => {
     ...get_roles(argv),
   ]
 
+  const expires_at = get_expiration(argv)
+
   /** @type {User} */
   const user = {
     roles,
   }
 
-  const jwt = sign({ secret }, user)
+  const jwt = sign({ secret, expires_at }, user)
 
   console.info(
     `${jwt}\n  ~~> http://localhost:5173/login?access_token=${jwt}\n  ~~> https://aptt.ch/login?access_token=${jwt}`,
