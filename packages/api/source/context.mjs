c21fdd001606f236a0e7d1e961b3655816be32f4
aptt <git@aptt.ch>
2023-09-03 13:58:14 +0200

    fix(api): 🐛 filter restricted elements in context function
    eliminates the need to check restrictions in every resolver. only what's
    allowed is available.

--- packages/api/source/context.mjs
+++ packages/api/source/context.mjs
@@ -13,6 +13,55 @@ assert(
   'missing `PRIVATE_JWT_SECRET` in environment.\n  ~~> $ echo PRIVATE_JWT_SECRET=replace_this >> .env',
 )
 
+/** @type {(restriction: Restriction, user?: { roles: string[] }) => boolean} */
+const meets_restriction = (restriction, user) => {
+  if (!user) {
+    return false
+  }
+
+  const has_all_roles = restriction.roles.every((role) => user.roles.includes(role))
+
+  return has_all_roles
+}
+
+/**
+ * @type {(options: {
+ *   user: User
+ *   technologies: Technology[]
+ *   projects: (Project & Restricted_Element & Content_Element)[]
+ *   activities: (Activity & Restricted_Element & Content_Element)[]
+ * }) => Resolver_Context}
+ */
+export const get_resolver_context = (context) => {
+  const { user, technologies } = context
+
+  const projects = context.projects.filter((project) => {
+    const meets_all_restrictions = project.restrictions.some((restriction) =>
+      meets_restriction(restriction, context.user),
+    )
+
+    return meets_all_restrictions
+  })
+
+  const activities = context.activities.filter((project) => {
+    const meets_all_restrictions = project.restrictions.some((restriction) =>
+      meets_restriction(restriction, context.user),
+    )
+
+    return meets_all_restrictions
+  })
+
+  /** @type {Resolver_Context} */
+  const resolver_context = {
+    user,
+    technologies,
+    projects,
+    activities,
+  }
+
+  return resolver_context
+}
+
 /**
  * @type {() => (
  *   initial_context: import('graphql-yoga').YogaInitialContext,
@@ -41,12 +90,7 @@ export const context_from_env = () => {
   const context_function = (initial_context) => {
     const user = get_user_from_request({ jwt_secret }, initial_context.request)
 
-    const context = {
-      projects,
-      technologies,
-      activities,
-      user,
-    }
+    const context = get_resolver_context({ user, technologies, projects, activities })
 
     return context
   }
